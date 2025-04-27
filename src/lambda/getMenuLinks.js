ac6766fb9d92881526e34062e7ca85a3f467211b
Jorgen <jorgen@janssonlee.com>
2025-04-27 18:08:04 +1000

    Section 4 completed

---
+++ src/lambda/getMenuLinks.js
@@ -0,0 +1,24 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const dynamodbClient = new DynamoDBClient({ region: "ap-southeast-2" });
+
+export const handler = async (event, context) => {
+  const params = {
+    TableName: "MenuLinks"
+  };
+
+  try {
+    const command = new ScanCommand(params);
+    const data = await dynamodbClient.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
